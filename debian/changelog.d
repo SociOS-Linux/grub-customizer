d1bb253970f251f53a7c85c16e01054603656d36
Daniel Richter <danielrichter2007@web.de>
2012-07-22 22:01:28 +0000

    translations updated

--- debian/changelog.d
+++ debian/changelog.d
@@ -1,6 +1,6 @@
 #!/bin/sh
 cat << EOF
-grub-customizer (2.0.4-0ubuntu1~ppa1$1) $2; urgency=medium
+grub-customizer (2.0.5-0ubuntu1~ppa1$1) $2; urgency=medium
 
    * settings manager included
    * 2.0.1: Bulgarian and Turkish translation updated
@@ -11,6 +11,7 @@ grub-customizer (2.0.4-0ubuntu1~ppa1$1) $2; urgency=medium
    * 2.0.4: chineese translation added, frensh and turkish translation updated
    * 2.0.4: fixed: timeout not settable when "show menu" not selected
    * 2.0.4: renamed "black" to "transparent" in background selection (highlight and normal)
+   * 2.0.5: translations updated
 
  -- Daniel Richter <danielrichter2007@web.de>  `date -R`
 
