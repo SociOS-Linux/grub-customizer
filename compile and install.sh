144864ad182aa518a1810f77bd1a2439499f33a0
Daniel Richter <danielrichter2007@web.de>
2012-07-22 22:00:49 +0000

    simple bugfix (sudo added)

--- compile and install.sh
+++ compile and install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cmake . && make && sudo cp --parents -u grub-customizer grubcfg-proxy /opt/grub-customizer/ && cp --parents translation/german.mo /opt/grub-customizer/
+cmake . && make && sudo cp --parents -u grub-customizer grubcfg-proxy /opt/grub-customizer/ && sudo cp --parents translation/german.mo /opt/grub-customizer/
