5904fe1c36100d65f674f2ed7400109cc84afef5
Daniel Richter <danielrichter2007@web.de>
2012-09-09 20:08:05 +0000

    crash if blkid prints devices without UUID... fixed

--- src/View/Gtk/Element/PartitionChooser.cpp
+++ src/View/Gtk/Element/PartitionChooser.cpp
@@ -49,11 +49,13 @@ void View_Gtk_Element_PartitionChooser::load(){
 		this->set_active(0);
 	}
 	for (Model_DeviceDataListInterface::const_iterator iter = deviceDataList->begin(); iter != deviceDataList->end(); iter++) {
-		Glib::ustring text = iter->first + "\n(" + (iter->second.find("LABEL") != iter->second.end() ? iter->second.at("LABEL") + ", " : "") + (iter->second.find("TYPE") != iter->second.end() ? iter->second.at("TYPE") : "") + ")";
-		uuid_map[text] = iter->second.at("UUID");
-		this->append_text(text);
-		if (strToLower(iter->second.at("UUID")) == strToLower(activePartition_uuid) || (activePartition_uuid == "" && iter == deviceDataList->begin() && !prependCurrentPartition)) {
-			this->set_active_text(text);
+		if (iter->second.find("UUID") != iter->second.end()) {
+			Glib::ustring text = iter->first + "\n(" + (iter->second.find("LABEL") != iter->second.end() ? iter->second.at("LABEL") + ", " : "") + (iter->second.find("TYPE") != iter->second.end() ? iter->second.at("TYPE") : "") + ")";
+			uuid_map[text] = iter->second.at("UUID");
+			this->append_text(text);
+			if (strToLower(iter->second.at("UUID")) == strToLower(activePartition_uuid) || (activePartition_uuid == "" && iter == deviceDataList->begin() && !prependCurrentPartition)) {
+				this->set_active_text(text);
+			}
 		}
 	}
 }
