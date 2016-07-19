3f049d6f78bf19720f851b2c1faf0786e9d4733b
Daniel Richter <danielrichter2007@web.de>
2016-07-19 17:50:25 +0000

    added include resolver

---
+++ build/resolve_includes.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+shopt -s globstar
+
+currentDir=$(dirname `readlink -f $0`)
+sourcePath="$currentDir/../src"
+
+if [ $# -ne 1 ] ; then
+	echo 'Error: You must run this command with exactly one argument containing the file path to be resolved' > /dev/stderr
+	exit 1
+fi
+
+fileToResolve="$1"
+
+declare -a files;
+files=();
+classes=();
+
+# ================= scan files =================
+
+IFS=$'\n'
+for file in "$sourcePath"/**/*.hpp "$sourcePath"/**/*.cpp ; do
+	for match in $(cat "$file" | grep -oE '^([\t ]*(class|struct) )[^ <>]+') ; do
+		classes+=("`echo -n "$match" | sed -r 's/^(class|struct) //'`");
+		files+=("${file:${#sourcePath} + 1}");
+	done;
+done;
+
+if [ ${#files[@]} -ne ${#classes[@]} ] ; then
+	echo 'E: arrays having different sizes';
+	exit 1;
+fi
+
+#for i in ${!files[@]}; do
+#	echo "${classes[$i]} -> ${files[$i]}";
+#done
+
+# ================= lookup =================
+
+declare -a resolvedIncludes;
+
+function isAlreadyAdded(){
+	local file="$1"
+
+	for existingFile in ${resolvedIncludes[@]}; do
+		if [ "$existingFile" == "$file" ] ; then
+			return 0
+		fi
+	done
+
+	return 1
+}
+
+function resolve(){
+	local file="$1"
+	local fileContent="$(cat "$sourcePath/$file")"
+	local i;
+	for i in ${!files[@]}; do
+		if echo -n "$fileContent" | grep -Eq "([^A-Za-z0-9_.]|^)${classes[$i]}([^A-Za-z0-9_.]|$)" ; then
+			if ! isAlreadyAdded "${files[$i]}" ; then
+				if [ "${files[$i]}" != "$file" ] ; then
+					#echo "[+] scanning children of ${files[$i]} (POS: $i)";
+					resolve "${files[$i]}"
+					#echo "[-] finished scanning children of ${files[$i]} (POS: $i)";
+				fi
+			
+				if ! isAlreadyAdded "${files[$i]}" ; then
+					resolvedIncludes+=("${files[$i]}")
+				fi
+			fi
+		fi
+	done
+}
+
+resolve "$fileToResolve"
+
+# ================= output =================
+
+for file in ${resolvedIncludes[@]}; do
+	echo "#include \"$file\"";
+done
