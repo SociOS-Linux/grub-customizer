d607cbfe283940eb915dd7327939fd1422e780f2
Daniel Richter <danielrichter2007@web.de>
2016-07-19 18:44:51 +0000

    added script to resolve all includes

---
+++ build/resolve_all_includes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+currentDir=$(dirname `readlink -f $0`)
+
+function resolve(){
+	local file="$1"
+	local extension="$2"
+	local prefix="$3";
+	echo "resolving $file.$extension"
+	"$currentDir/resolve_includes.sh" "$file.$extension" "$prefix" > "$currentDir/../src/$file.inc.$extension";
+}
+
+resolve "Bootstrap/GtkApplication" "cpp" ../
+resolve "Bootstrap/FactoryImpl/GlibThread" "cpp" ../../
+resolve "Bootstrap/FactoryImpl/GLibRegex" "cpp" ../../
+resolve "Bootstrap/FactoryImpl/GlibThread" "cpp" ../
+resolve "Bootstrap/GtkView" "cpp" ../
+resolve "main/proxy" "cpp" ../
+resolve "main/client" "cpp" ../
