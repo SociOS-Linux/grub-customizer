d1f69d6f7ffd3c82640ffa9544b2a7bb45a22502
Daniel Richter <danielrichter2007@web.de>
2016-07-21 21:29:23 +0000

    added c++ implementation of include resolver

---
+++ build/resolve_includes.cpp
@@ -0,0 +1,151 @@
+#include <fstream>
+#include <list>
+#include <iostream>
+#include <unistd.h>
+#include <cstring>
+#include <glob.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <regex>
+#include <algorithm>
+
+std::string get_directory(std::string argv0)
+{
+	auto readlink = popen(("dirname \"`readlink -f \"" + std::string(argv0) + "\"`\"").c_str(), "r");
+
+	std::string path;
+	int c = 0;
+	while ((c = fgetc(readlink)) != EOF) {
+		if (c == '\n') {
+			continue;
+		}
+		path += char(c);
+	}
+	pclose(readlink);
+
+	return path;
+}
+
+std::list<std::string> getFiles(std::string directory, std::list<std::string> fileExtensions) {
+	std::list<std::string> result;
+
+	DIR* dir = opendir(directory.c_str());
+
+	dirent* file = nullptr;
+	while ((file = readdir(dir)) != nullptr) {
+		std::string fileName = file->d_name;
+
+		if (fileName == "." || fileName == "..") {
+			continue;
+		}
+		if (file->d_type == DT_DIR) {
+			result.splice(result.end(), getFiles(directory + "/" + fileName, fileExtensions));
+		} else {
+			for (auto extension : fileExtensions) {
+				if (fileName.substr(fileName.size() - extension.size()) == extension) {
+					result.push_back(directory + "/" + fileName);
+					break;
+				}
+			}
+		}
+	}
+
+	return result;
+}
+
+std::string readFile(std::string file)
+{
+	std::ifstream ifs(file.c_str(), std::ios::in | std::ios::binary | std::ios::ate);
+
+	std::ifstream::pos_type fileSize = ifs.tellg();
+	ifs.seekg(0, std::ios::beg);
+
+	std::vector<char> bytes(fileSize);
+	ifs.read(&bytes[0], fileSize);
+
+	return std::string(&bytes[0], fileSize);
+}
+
+class Resolver
+{
+	public: std::list<std::string> files;
+	public: std::list<std::string> classes;
+	public: std::string sourcePath;
+
+	public: std::list<std::string> resolvedIncludes;
+	public: std::list<std::string> dispatchedIncludes;
+
+	public: void scanFiles()
+	{
+		for (auto file : getFiles(this->sourcePath, {"hpp", "cpp"})) {
+			std::string fileContent = readFile(file);
+			std::regex regex("(?:^|\n)(?:[\\t ]*(?:class|struct) )([^ <>\n]+)");
+
+			std::smatch matches;
+			std::string::const_iterator searchStart(fileContent.cbegin());
+			while (std::regex_search(searchStart, fileContent.cend(), matches, regex)) {
+				this->files.push_back(file.substr(this->sourcePath.size() + 1));
+				this->classes.push_back(matches[1]);
+				searchStart += matches.position() + matches.length();
+			}
+		}
+	}
+
+	public: void resolve(std::string fileToResolve)
+	{
+		std::string fileContent = readFile(this->sourcePath + "/" + fileToResolve);
+
+		auto fileIter = this->files.begin();
+		auto classIter = this->classes.begin();
+		for (int i = 0; i < this->files.size(); i++) {
+			std::regex regex("([^A-Za-z0-9_.]|^|\n)" + *classIter + "([^A-Za-z0-9_.]|$|\n)");
+
+			if (std::regex_search(fileContent, regex)) {
+				if (std::count(this->dispatchedIncludes.begin(), this->dispatchedIncludes.end(), *fileIter) == 0) {
+					this->dispatchedIncludes.push_back(*fileIter);
+					if (*fileIter != fileToResolve) {
+						std::cerr << "[+] scanning children of " << *fileIter << " (POS: " << i << ")" << std::endl;
+						resolve(*fileIter);
+						std::cerr << "[-] finished scanning children of " << *fileIter << " (POS: " << i << ")" << std::endl;
+					}
+
+					if (std::count(this->resolvedIncludes.begin(), this->resolvedIncludes.end(), *fileIter) == 0
+						&& *fileIter != fileToResolve) { // TODO: camparing global file to resolve really required?
+
+						std::cerr << "adding " << *fileIter << std::endl;
+						this->resolvedIncludes.push_back(*fileIter);
+					}
+				}
+			}
+			fileIter++;
+			classIter++;
+		}
+	}
+};
+
+int main(int argc, char** argv)
+{
+	std::string currentDir = get_directory(argv[0]);
+
+
+	if (argc != 3) {
+		std::cerr << "Error: wrong argument count" << std::endl;
+		return 1;
+	}
+
+	Resolver resolver;
+
+	std::string fileToResolve = argv[1];
+	std::string prefix = argv[2];
+
+	resolver.files = {"lib/Helper.hpp"};
+	resolver.classes = {"assert"};
+	resolver.sourcePath = currentDir + "/../src";
+
+	resolver.scanFiles();
+	resolver.resolve(fileToResolve);
+
+	for (auto file : resolver.resolvedIncludes) {
+		std::cout << "#include \"" << prefix << file << "\"" << std::endl;
+	}
+}
