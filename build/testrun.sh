1c81b477615e94765bf32678aa7dac088b072606
Daniel Richter <danielrichter2007@web.de>
2016-08-14 00:20:10 +0200

    added testrun script

---
+++ build/testrun.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# compile, install and run this project for testing
+
+set -e
+
+currentDir=$(dirname `readlink -f $0`)
+
+if ! [ -e "$currentDir/tmp" ] ; then
+    mkdir "$currentDir/tmp";
+fi
+
+cd "$currentDir/tmp"
+
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX="$currentDir/tmp" "$currentDir/.."
+make install
