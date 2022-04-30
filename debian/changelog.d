03c5f5fac82d9e4004164a0172902df79fbe78b5
daniel <daniel@daniel-VirtualBox>
2022-04-30 19:20:07 +0200

    changed version to 5.2.0

--- debian/changelog.d
+++ debian/changelog.d
@@ -1,5 +1,11 @@
 #!/bin/sh
 cat << EOF
+grub-customizer (5.2.0-0ubuntu1~ppa1$1) $2; urgency=low
+
+   * new upstream release
+
+ -- Daniel Richter <danielrichter2007@web.de>  Sat, 30 Apr 2022 19:17:00 +0200
+
 grub-customizer (5.1.0-0ubuntu1~ppa1$1) $2; urgency=low
 
    * new upstream release
